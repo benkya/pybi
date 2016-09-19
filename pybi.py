ce764af697bacc7f5527306cb9f99ed02d632760
ben.chaung <ben.chaung@hotmail.com>
2016-09-19 18:24:37 +0800

    最近的更改

--- pybi.py
+++ pybi.py
@@ -11,7 +11,7 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
-import sys
+dmport sys
 reload(sys)
 
 sys.setdefaultencoding('utf-8')
